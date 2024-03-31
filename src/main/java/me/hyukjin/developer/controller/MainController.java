b36feb6d9c1eba0b27a22c1579f915e1be74a236
leehj <sksdlwoasl@naver.com>
2024-03-31 18:59:28 +0900

    2023-03-31 프로젝트 구성

---
+++ src/main/java/me/hyukjin/developer/controller/MainController.java
@@ -0,0 +1,13 @@
+package me.hyukjin.developer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public String home(){
+        return "hello, world";
+    }
+}
